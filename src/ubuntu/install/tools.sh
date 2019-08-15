a65c6c267c3a61d0f9a0ce25b97705138880b640
Tien Tran <tiendung.tran@gmail.com>
2019-08-15 13:54:02 +1000

    Install gosu curl git ssh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -6,6 +6,16 @@ echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy #used for websockify/novnc
+
+apt-get install curl git ssh
+
+# install gosu
+dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')" && \
+curl -fsLo /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/1.11/gosu-$dpkgArch" && \
+chmod +x /usr/local/bin/gosu && \
+gosu nobody true
+# complete gosu
+
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
